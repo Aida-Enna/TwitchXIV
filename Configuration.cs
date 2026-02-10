ae8aecb3b82a68fd7bef783cd68adac36e8a7bb7
= <=>
2026-02-09 19:11:10 -0600

    Update default chat color settings in Configuration

--- Configuration.cs
+++ Configuration.cs
@@ -12,9 +12,9 @@ namespace TwitchXIV
         public bool TwitchEnabled = true;
         public int MaxChatlines = 500;
         public Vector4 BackgroundColor = new(0f, 0f, 0f, 0.4f);
-        public Vector4 TimeColor = new(0.7f, 0.7f, 0.7f, 1f);
-        public Vector4 ChannelColor = new(1.0f, 0.8f, 0.4f, 1f);
-        public Vector4 MessageColor = new(0.9f, 0.9f, 0.9f, 1f);
+        public Vector4 TimeColor = new(1f, 1f, 1f, 1f);
+        public Vector4 ChannelColor = new(0.4f, 1f, 1f, 1f);
+        public Vector4 MessageColor = new(0.78f, 0.78f, 0.78f, 1f);
         public Vector4 HighlightColor = new(1f, 1f, 1f, 0.1f);
     }
 }
